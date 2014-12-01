8c184727113f8ffc892c726f3a1fb15849db2e87
mthoretton <mathieu@thoretton.com>
2014-12-01 09:15:22 +0100

    Add doSysCallReboot

--- syscall.c
+++ syscall.c
@@ -4,3 +4,15 @@ void __attribute__ ((naked)) SWIHandler (int sig)
 {
 	
 }
+
+void doSysCallReboot () {
+	const int PM_RSTC = 0x2010001c;
+	const int PM_WDOG = 0x20100024;
+	const int PM_PASSWORD = 0x5a000000;
+	const int PM_RSTC_WRCFG_FULL_RESET = 0x00000020;
+	
+	PUT32(PM_WDOG, PM_PASSWORD | 1);
+	PUT32(PM_RSTC, PM_PASSWORD | PM_RSTC_WRCFG_FULL_RESET);
+
+	while (1);
+}
