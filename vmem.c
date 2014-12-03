2a9d9c98bac2115caf48d2a9f83d19b0a258efec
LeonardA-L <leonard.allain-launay@insa-lyon.fr>
2014-12-03 08:55:20 +0100

    Adding translation table initialization. Not compiled, Not tested

--- vmem.c
+++ vmem.c
@@ -9,8 +9,46 @@
 #define	SECTION_SIZE	(SECON_LVL_TT_COUN * PAGE_SIZE)		// 
 #define TOTAL_TT_SIZE	(FIRST_LVL_TT_SIZE + SECON_LVL_TT_SIZE * FIRST_LVL_TT_COUN)			// 42Mo
 
-#define PAGE_TABLE_START_ADDR	0x48001
+#define PAGE_TABLE_START_ADDR	(0x48000)
+#define SECOND_TABLE_START_ADDR (PAGE_TABLE_START_ADDR+FIRST_LVL_TT_SIZE)
+
+void* computePageAddr(int first_level_idx, int second_level_idx){
+	return (i<<20)+(j<<12);
+}
+
 
 unsigned int init_kern_translation_table(void){
-	//for(void* p = 0x0; p<0x50000
+	uint32_t device_flags = 0b00000111111;
+	uint32_t t1_flags = 0b;
+	// Init first level tables
+	int i=0;
+	for(uint32_t* p = PAGE_TABLE_START_ADDR; p<(PAGE_TABLE_START_ADDR+FIRST_LVL_TT_SIZE);p++)
+	{
+		// Init second level tables
+		int j=0;
+		for(uint32_t* q=SECOND_TABLE_START_ADDR + i*SECON_LVL_TT_SIZE; q<(SECOND_TABLE_START_ADDR + (i+1)*SECON_LVL_TT_SIZE); q++)
+		{
+			
+			// Add entry to second level table
+			void* page_addr = computePageAddr(i,j);
+			if(page_addr < 0x500000)
+			{
+				*q = page_addr | 0b00000111110; 	// same as devices for the moment
+			}
+			else if(page_addr > 0x20000000 && page_addr < 0x20FFFFFF )
+			{
+				*q = page_addr | device_flags;
+			}
+			else
+			{
+				// Translation fault
+				*q = Ob00;
+			}
+			j++;
+		}
+		
+		// Add entry to first level table
+		p = (SECOND_TABLE_START_ADDR + i*SECON_LVL_TT_SIZE) | t1_flags;
+		i++;
+	}
 }
